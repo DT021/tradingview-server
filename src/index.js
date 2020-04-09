9bc159e85163c9fd09ec08b660439e022b3aa1d2
Saim <saimfd00@gmail.com>
2020-04-09 23:12:40 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,57 +1,55 @@
 const express = require("express")
 const app = express()
 const r = require("rethinkdb");
-const fs = require('fs');
-const https = require('https');
 const dotenv = require('dotenv').config();
+const https = require('https');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const io = require('socket.io')(7001, {
     pingTimeout: 60000
 });
-app.use(express.static('public'));
+
 app.use(bodyParser.json());
 app.use(cors());
 
 io.on('connection', (socket) => {
     socket.on('start-socket', (market) => {
         r.connect({
-            host: "35.180.0.174",
+            host: "34.220.23.203",
             port: 28015
         }).then((conn) => {
-            // r.db("brain_markets").table(`${market}_signals`).changes().run(conn).then((cursor) => {
-            //     cursor.each((err, row) => {
-            //         console.log(row)
-            //         // socket.emit('chart-data', row.new_val)
-            //         if(row.new_val.type == 'buy'){
-            //             socket.emit('buy-signal', row.new_val);
-            //         }
-            //         if(row.new_val.type == 'sell'){
-            //             socket.emit('sell-signal', row.new_val)
-            //         }
-            //     })
-            // }).catch((e) => {
-            //     console.log("Table")
-            // })
+            r.db("brain_markets").table(`${market}_signals`).changes().run(conn).then((cursor) => {
+                cursor.each((err, row) => {
+                    if(!row.new_val){
+                        cursor.close();
+                    }
+                    socket.emit('signal', row.new_val)
+                }).catch(() => {
+                    console.log("Table Removed.")    
+                })
+            }).catch((e) => {
+                console.log("Table Removed.")
+            })
         })
     })
 })
 console.log("SOCKET LISTENING ON 7001")
 
-r.connect({
-    host: "35.180.0.174",
-    port: 28015
-}).then((conn) => {
-    r.db('brain_markets').table('smort_chart_signals').run(conn).then((cur) => {
-        cur.toArray().then((data) => {
-            console.log(data)
-        })
-    })
-})
+// r.connect({
+//     host: "34.220.23.203",
+//     port: 28015
+// }).then((conn) => {
+//     r.db('brain_markets').table('smort_chart_signals').run(conn).then((cur) => {
+//         cur.toArray().then((data) => {
+//             console.log(data)
+//         })
+//     })
+// })
 
 app.get("/api/markets", (req, res) => {
     r.connect({
-        host: "35.180.0.174",
+        host: "34.220.23.203",
         port: 28015
     }).then((conn) => {
         r.db("brain_markets").table('markets').run(conn).then((cur) => {
@@ -67,7 +65,7 @@ app.post("/api/market", (req, res) => {
     let name = body.name.toLowerCase().replace(/[/ ]/g, "_");
     console.log(name)
     r.connect({
-        host: "35.180.0.174",
+        host: "34.220.23.203",
         port: 28015
     }).then((conn) => {
         r.db("brain_markets").table('markets').insert({
@@ -95,7 +93,7 @@ app.post("/api/market", (req, res) => {
 app.delete("/api/market", (req, res) => {
     let id = req.query.id;
     r.connect({
-        host: "35.180.0.174",
+        host: "34.220.23.203",
         port: 28015
     }).then((conn) => {
         r.db("brain_markets").table('markets').get(id).run(conn).then((doc) =>{
@@ -116,7 +114,7 @@ app.delete("/api/market", (req, res) => {
 app.get('/api/market', (req, res) => {
     let market = req.query.market;
     r.connect({
-        host: "35.180.0.174",
+        host: "34.220.23.203",
         port: 28015
     }).then((conn) => {
         r.db("brain_markets").table(market).run(conn).then((data) => {
@@ -130,10 +128,10 @@ app.get('/api/market', (req, res) => {
 app.get('/api/signals', (req, res) => {
     let market = req.query.market;
     r.connect({
-        host: "35.180.0.174",
+        host: "34.220.23.203",
         port: 28015
     }).then((conn) => {
-        r.db("brain_markets").table(`${market}_signals`).distinct().limit(200).run(conn).then((data) => {
+        r.db("brain_markets").table(`${market}_signals`).orderBy({index: r.desc('time')}).limit(200).run(conn).then((data) => {
             data.toArray().then((data) => {
                 res.send(data)
             })
@@ -142,18 +140,12 @@ app.get('/api/signals', (req, res) => {
 })
 
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-})
+// serve the API with signed certificate on 443 (SSL/HTTPS) port
+const httpsServer = https.createServer({
+  key: fs.readFileSync(`${__dirname}/sslcert/selfsigned.key`),
+  cert: fs.readFileSync(`${__dirname}/sslcert/selfsigned.crt`),
+}, app);
 
-var key = fs.readFileSync(__dirname + '/sslcert/selfsigned.key');
-var cert = fs.readFileSync(__dirname + '/sslcert/selfsigned.crt');
-var options = {
-  key: key,
-  cert: cert
-};
-
-var server = https.createServer(options, app);
-server.listen(7000, () => {
+httpsServer.listen(7000, () => {
     console.log(`Server started on Port 7000`)
 })
